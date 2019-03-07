0f788c1df4e433756e094c9dd8cffcc79ae530f1
Matthias Flath <MatthiasGreen@gmail.com>
2019-03-07 13:34:56 -0600

    more updates

--- src/finalProject/Sprite.java
+++ src/finalProject/Sprite.java
@@ -21,18 +21,6 @@ public class Sprite {
 	public void setY(int yPosition) {
 		this.yPosition = yPosition;
 	}
-	public int getI_width() {
-		return i_width;
-	}
-	public void setI_width(int i_width) {
-		this.i_width = i_width;
-	}
-	public int getI_height() {
-		return i_height;
-	}
-	public void setI_height(int i_height) {
-		this.i_height = i_height;
-	}
 	public Image getImage() {
 		return image;
 	}
