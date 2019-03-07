9f8ea89adcb52464f40736a05934ab2d14389448
Matthias Flath <MatthiasGreen@gmail.com>
2019-03-07 09:46:41 -0600

    Added a Room Class for the gamespace and a sprite class.

---
+++ src/finalProject/Room.java
@@ -0,0 +1,15 @@
+package finalProject;
+
+public class Room {
+	
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 800;
+	public static final int BOTTOM = 790;
+	public static final int START_PADDLE_X = 300; 
+	public static final int START_PADDLE_Y = 760;
+	public static final int START_BALL_X = 300;
+	public static final int START_BALL_Y = 400;
+	public static final int START_DELAY = 1000;
+	public static final int STEP_TIME = 10;
+ 
+}
---
+++ src/finalProject/Sprite.java
@@ -0,0 +1,11 @@
+package finalProject;
+
+import java.util.*;
+
+public class Sprite {
+	
+	private int x, y, i_width, i_height;
+	private Image image;
+	
+
+}
